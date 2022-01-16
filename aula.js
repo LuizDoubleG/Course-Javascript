14640aaa6bfaa3491bcfaf0051c2f02a11491739
LuizDoubleG <20201100016@pq.uenf.br>
2022-01-16 15:53:40 -0300

    feat(.js): changed aula.js to learning.js and added new commands

--- aula.js
+++
@@ -1,6 +0,0 @@
-/*var*/
-let idade = 7; /*;*/
-console.log(idade);
-console.log('stuff');
-//camelCase
-//ctrl+; comenta tudo selecionado
---
+++ learning.js
@@ -0,0 +1,19 @@
+// /*var*/
+// const age = 7; /*;*/
+// console.log(age);
+// console.log('stuff');
+// //camelCase
+// //ctrl+; comments all selected
+// /*const = constant doesn't change -> error  
+// while trying to chagne the valor*/
+let Name = 'Luiz'; // string literal
+console.log(Name);
+let age = 19.6; // number literal
+console.log(age);
+let aprovation = true; //boolean
+console.log(aprovation);
+let secondName; // Undefined default
+// let secondName = undefined -> less used
+let selectedColor = null // redefine a value
+ // null = resettable
+ // so, what's on front of a variable defines it
