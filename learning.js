2f91f7909f18224b8095d0d64ed8e3e69af62b6b
LuizDoubleG <20201100016@pq.uenf.br>
2022-01-30 23:03:06 -0300

    added(lesson 4): objects

--- learning.js
+++ learning.js
@@ -5,17 +5,24 @@
 // //camelCase
 // //ctrl+; comments all selected
 // /*const = constant doesn't change -> error  
-// while trying to chagne the valor*/
-let Name = 'Luiz'; // string literal
-console.log(Name);
-let age = 19.6; // number literal
-console.log(age);
-let aprovation = true; //boolean
-console.log(aprovation);
-let secondName; // Undefined default
+// while trying to change the valor*/
+// let Name = 'Luiz'; // string literal
+// console.log(Name);
+// let age = 19.6; // number literal
+// console.log(age);
+// let aprovation = true; //boolean
+// console.log(aprovation);
+// let secondName; // Undefined default
 // let secondName = undefined -> less used
-let selectedColor = null // redefine a value
- // null = resettable
- // so, what's on front of a variable defines it
-// javascript is dynamic 
+// let selectedColor = null // redefine a value
+// null = resettable
+// so, what's on front of a variable defines it
+// javascript types are dynamic 
 //change
+
+let person = {
+    Name: 'Luiz',
+    age: 19,
+    aprovation: true,
+    secondName: 'Miguel'
+};
