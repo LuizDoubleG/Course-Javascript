750a7176fa486a3521a47a9133839ff3450df0f5
LuizDoubleG <20201100016@pq.uenf.br>
2022-01-31 00:33:31 -0300

    added(lesson 7): function types

--- learning.js
+++ learning.js
@@ -53,3 +53,23 @@ function resetColor(hue, color){ //() <- parameters
 console.log(siteColor);
 resetColor("bright"," red"); // only with red -> redundefined
 console.log(siteColor);
+
+
+//empty functions only prints
+//functions can use return commands
+
+function printName(){
+    console.log('Miguel');
+};
+
+printName();
+
+function multiplyByTwo(value){
+    return value* 2;
+};
+
+console.log(multiplyByTwo(7)); //we used console.log because the function multiplyByTwo doesn't print anything only return values!
+
+let result = multiplyByTwo(12);
+
+console.log(result);
