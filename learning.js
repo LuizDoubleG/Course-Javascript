468f87a86c18a75b68a61b679ddaa8d3c8f148d3
LuizDoubleG <20201100016@pq.uenf.br>
2022-01-30 23:37:53 -0300

    added(lesson 5): arrays

--- learning.js
+++ learning.js
@@ -26,3 +26,12 @@ let person = {
     aprovation: true,
     secondName: 'Miguel'
 };
+
+//Arrays -> united data acessed by index
+
+let family = [19,51,'stuff',true,49,9];
+//console.log(family[3]);
+console.log(family.length);
+
+let friend = ['Rafael',19,'Rio de Janeiro'];
+console.log(friend);
