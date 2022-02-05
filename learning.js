d924f07f87e38fddd377b7c004cba4722a76325e
LuizDoubleG <20201100016@pq.uenf.br>
2022-02-05 13:51:49 -0300

    added(lesson 9): If...Else

--- learning.js
+++ learning.js
@@ -145,5 +145,32 @@ customizedColor = '';
 profileColor = customizedColor || defaultColor;
 console.log(profileColor); // -> Blue
 
+// If..Else
+// If time is between 06:00 untill 12:00 = Good morning!
+// If time is between 12:00 untill 18:00 = Good afternoon!
+// Else = Good night!
+
+/* if (condition) {
+    script to be executed
+}
+else if (anotherCondition){
+    script to be executed
+}
+else {
+    script to be executed
+}
+*/
+
+let time = 5;
+
+if (time >= 6 && time < 12) {
+    console.log("Good morning!");
+}
+else if (time >= 12 && time < 18){
+    console.log("Good afternoon!");
+}
+else{
+    console.log("Good night!");
+}
 // Bitwise
 
