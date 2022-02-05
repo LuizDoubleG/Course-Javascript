895f1e9c1662f3c78cd70818a64e3834a31be9ff
LuizDoubleG <20201100016@pq.uenf.br>
2022-02-05 14:43:15 -0300

    added(lesson 10): Switch...case

--- learning.js
+++ learning.js
@@ -172,5 +172,31 @@ else if (time >= 12 && time < 18){
 else{
     console.log("Good night!");
 }
-// Bitwise
 
+//switch case
+ /* switch (variable){
+    case (condition):
+         execute script
+    break; (to exit condition comparison) 
+ }
+ */
+let permission;
+permission = 'manager'
+switch (permission){
+    case 'default':
+        console.log('Default User');
+    break;
+
+    case 'manager':
+        console.log('Manager User');
+    break;
+
+    case 'principal':
+        console.log('Principal User');
+    break;
+
+    default:
+        console.log('Undefined User');
+}
+
+// Bitwise
