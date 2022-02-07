e1163eb1f795e80ccaf8ac2cadc97f630c54ebcd
LuizDoubleG <20201100016@pq.uenf.br>
2022-02-07 20:37:18 -0300

    added(lesson 11): Loop types

--- learning.js
+++ learning.js
@@ -43,7 +43,7 @@ console.log(friend);
 */
 
 //create functions (extructure): Verb + Substantive
-
+/*
 let siteColor = "blue";
 
 function resetColor(hue, color){ //() <- parameters
@@ -53,7 +53,7 @@ function resetColor(hue, color){ //() <- parameters
 console.log(siteColor);
 resetColor("bright"," red"); // only with red -> redundefined
 console.log(siteColor);
-
+*/
 
 //empty functions only prints
 //functions can use return commands
@@ -99,12 +99,15 @@ console.log('1' == 1); //true (complex convertion not recommended to use)
 */
 
 // Ternary operator
+/*
 let score = 101;
 let typeClient = score > 100 ? 'premium' : 'common';
 console.log(typeClient);
+*/
 
 // Logic
 // and (&&)
+/*
 let age = 17;
 let major = age >= 18; 
 let hasWorkCard = true;
@@ -119,6 +122,8 @@ console.log('Can apply for the job: ',canJoinJob);
 // Not (!)
 let applicantRefused = !canJoinJob;
 console.log('Applicant Refused: ', applicantRefused);
+*/
+
 // Non-boolean comparison 
 /*  Falsy
     undefined
@@ -136,6 +141,8 @@ console.log('Applicant Refused: ', applicantRefused);
     (outputs first truthy value)
     example:
 */
+
+/*
 let customizedColor = 'Red';
 let defaultColor = 'Blue';
 let profileColor = customizedColor || defaultColor; //(defines profileColor by wich truthy value comes first)
@@ -144,6 +151,7 @@ console.log(profileColor); // -> Red
 customizedColor = '';
 profileColor = customizedColor || defaultColor;
 console.log(profileColor); // -> Blue
+*/
 
 // If..Else
 // If time is between 06:00 untill 12:00 = Good morning!
@@ -161,6 +169,7 @@ else {
 }
 */
 
+/*
 let time = 5;
 
 if (time >= 6 && time < 12) {
@@ -172,6 +181,7 @@ else if (time >= 12 && time < 18){
 else{
     console.log("Good night!");
 }
+*/
 
 //switch case
  /* switch (variable){
@@ -180,6 +190,7 @@ else{
     break; (to exit condition comparison) 
  }
  */
+/*
 let permission;
 permission = 'manager'
 switch (permission){
@@ -197,6 +208,77 @@ switch (permission){
 
     default:
         console.log('Undefined User');
+}
+*/
+// Loops
+// 1.For
+for(let i = 0; i < 5; i++){
+    console.log('Im Learning',i);
+}
+for(i = 1; i <= 5; i++){
+    if(i%2 !== 0){
+        console.log(i);//prints only odd numbers
+    }
+}
+
+for(i = 5; i >= 0; i--){
+    console.log(i);
+}
+
+// 2.While loop
+i = 5
+while (i >= 1) {
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+    i--;
+}
+
+// 3.Do...While
+i = 0
+do {
+    console.log('typing!');
+    i++;
+} while (i<10) //typing!
+/* difference between while and do...while 
+-> do...while always execute the code atleast 1 time before checking if de condition is right
+   least used
+*/
+
+// 4.For-in (used for iterate(repeat) object properties or array elements)
+//object
+const person = {
+    /*Key*/Name: 'Miguel',/*value*/
+    age: 19
+};
+
+for (let key in person) {
+    console.log(key, person[key]); // Name and age -> key
+} // "increments" key untill the end of object indexes
+
+//dot notation -> person.Name (when you know the propertie name) or
+//person['Name'] -> prints Name: Miguel age: Miguel (wrong)
+
+const colors = ['Red', 'Blue', 'Green'];
+
+for (let index in colors){
+    console.log(index,colors[index]);
+}
+//0 Red
+//1 Blue
+//2 Green
+
+// 5.For...of
+// iterates upon array itens
+
+for(let color of colors){
+    console.log(color);
+}
+// prints only colors without index
+// variable color vanishes 
+// E.g.:
+for(let datas of person){
+    console.log(datas);
 }
 
 // Bitwise
