e7f3dcc94c3f7787b111de401a122b04dba59374
LuizDoubleG <20201100016@pq.uenf.br>
2022-02-23 16:48:00 -0300

    added(challenge 4): Even or Odd

--- learning.js
+++ learning.js
@@ -401,4 +401,15 @@ function checkSpeed(speed){
 }
 
 
+//Even or Odd
+//Recieves a max number of values to evaluate if they are even or odd
+//Iterates over each value
+showType(5);
+
+function showType(limit){
+    for(let i = 0; i <= limit; i++){
+        i % 2 === 0 ? console.log(i, "EVEN") : console.log(i, "ODD");
+    }
+}
+
 // Bitwise
