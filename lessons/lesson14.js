1f5e05b0cdfdc99edb3d46cc38a82c77f949ecdb
LuizDoubleG <20201100016@pq.uenf.br>
2022-03-01 10:43:47 -0300

    fixed(lesson 14): lesson14.js missing content

---
+++ lessons/lesson14.js
@@ -0,0 +1,14 @@
+// Lesson 14: dynamic nature of objects in JavaScript
+const mouse = {
+    color: 'Green',
+    brand: 'Razzer'
+};
+mouse.speed = 5000;
+mouse.changeDPI = function() {
+    console.log('Changing DPI...');
+}
+console.log(mouse); 
+
+delete mouse.speed;
+delete mouse.changeDPI;
+console.log(mouse);
