8b111a6bc55ad6d54c95ccaad05a8a56461d9427
LuizDoubleG <20201100016@pq.uenf.br>
2022-03-01 09:00:16 -0300

    added(lesson 12): Factory functions

--- learning.js
+++ learning.js
@@ -553,7 +553,7 @@ function showAsterisk(lines) {
 //End of challenge 8
 
 //Challenge 9:
-
+//Prints every single Prime Number untill the declaredlimit 
 primeNumbers(15);
 
 /*
@@ -602,9 +602,78 @@ function checkPrimeNumber(number) {
     }
     return true;
 } 
-
-
-
-
 //End of challenge 9
+
+//Lesson 12: Factory functions
+//Several objects 
+//ex.:
+//full object with function and specifications:
+// const cellPhone = {
+//     brand : 'SAMSUNG',
+//     screenSize : {
+//         vertical : 155,
+//         horizontal : 75
+//     },
+//     batteryCapacity : 5000,
+//     call : function() {
+//         console.log("Making phone call...")
+//     }
+// }
+//If we want 100 objects like that, one option is:
+//repeat it 100x
+//instead:
+//function createCellPhone() {
+function createCellPhone (brand, screenSize, batteryCapacity) {
+    // const cellPhone = {
+    //     brand : 'SAMSUNG',
+    //     screenSize : {
+            //vertical : 155,
+            //horizontal : 75
+        //},
+        //batteryCapacity : 5000,
+        //call : function() {
+            //console.log("Making phone call...")
+        //}
+    //}
+    //return cellPhone;
+    //(tip: comment all = ctrl + ;)
+   //or:
+    //return {
+        //brand : 'SAMSUNG',
+        //screenSize : {
+            //vertical : 155,
+            //horizontal : 75
+        //},
+        //batteryCapacity : 5000,
+        //call : function() {
+            //console.log("Making phone call...")
+        //}
+    //}
+    //This is Hard Coding, that means it can't be parameterized
+    // it isn't dynamic 
+    // with function createCellPhone(...,...,...) {
+    // and this:
+    //return {
+        //brand : brand,
+        //screenSize : screenSize,
+        //batteryCapacity : batteryCapacity,
+        //call : function() {
+            //console.log("Making phone call...")
+        //}
+    //}
+   //or:
+    return {
+        brand,
+        screenSize,
+        batteryCapacity,
+        call () {
+            console.log("Making phone call...")
+        }
+    }
+    // Now it is dynamic and parameterizable
+}
+
+const cellPhone1 = createCellPhone('Xiaomi', 5.5, 5000);
+console.log(cellPhone1);
+
 // Bitwise
